e7fe6b6d93e7606eabf05fcca5dfce053841933a
Ryan Davis <ryan@acceleration.net>
2015-05-05 22:35:38 -0400

    solutions for ch02ex03

--- src/ch02/ex03.js
+++ src/ch02/ex03.js
@@ -1,7 +1,15 @@
 function ch02ex03a() {
-
+  return ch02ex03b(8);
 }
 
 function ch02ex03b(size) {
-
+  var result = '', chars = ' #';
+  for(var i = 0; i < size; i++){
+    var offset = i % 2;
+    for(var j = 0; j < size; j++){
+      result += chars[(offset + j)%2];
+    }
+    result += '\n';
+  }
+  return result;
 }
