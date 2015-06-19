f5ddde1c4a322578974e2e025c86f9934ab104ee
Ryan Davis <ryan@acceleration.net>
2015-06-18 20:47:12 -0400

    ch06: implement Vector
    I don't really like javascript's OO. The `this` keyword seems too
    noisy. I think C# gets away with it by making `this` implicit. Scanning
    the length function, my eyes just see "thisthisthisthis" and it's
    distracting from the work actually being done.

--- src/ch06/Vector.js
+++ src/ch06/Vector.js
@@ -13,4 +13,23 @@
  * the origin (0, 0).
  */
 
-function Vector(){}
+function Vector(x, y){
+  this.x = x;
+  this.y = y;
+}
+
+Vector.prototype.plus = function(vec) {
+  return new Vector(this.x + vec.x, this.y + vec.y);
+};
+
+Vector.prototype.minus = function(vec) {
+  return new Vector(this.x - vec.x, this.y - vec.y);
+};
+
+Object.defineProperty(
+  Vector.prototype, 'length',
+  {
+    get: function(){
+      return Math.sqrt(this.x*this.x + this.y*this.y);
+    }
+  });
