b367eef63c3aa63e2ade91a68a87d56eb94ef840
Ryan Davis <ryan@mokeys.org>
2015-07-15 22:14:33 -0400

    ch08 stubs and custom test matcher
    Adds a few jasmine matchers:
    
    * `expect(regex).toMatchAll(...)`
    * `expect(regex).toMatchNone(...)`
    
    both take variable arguments for strings the given regex should or
    should not match. I couldn't really do this as one matcher because "not
    all of these match" is different from "all of these don't match"
    
    Also, jasmine already has `expect(string).toMatch(regex)`, so I needed a
    name for my custom matcher.

---
+++ src/ch09/regexMatches.js
@@ -0,0 +1,39 @@
+var regexMatchers = {
+  toMatchAll: function(){
+    return {
+      compare: function(regex) {
+        var unmatched = Array.prototype.slice.call(arguments, 1);
+
+        if(regex)
+          unmatched = unmatched.filter(function(s) { return !regex.test(s); });
+
+        if(unmatched.length == 0) return { pass: true };
+
+        return {
+          pass: false,
+          message: 'Failed to match: `' + unmatched.join("`, `") + '`'
+        };
+      }
+    };
+  },
+  toMatchNone: function() {
+    return {
+      compare: function(regex) {
+        var matched = Array.prototype.slice.call(arguments, 1);
+
+        if(regex)
+          matched = matched.filter(regex.test);
+
+        if(matched.length == 0) return { pass: true };
+
+        return {
+          pass: false,
+          message: 'Incorrectly matched: `' + matched.join("`, `") + '`'
+        };
+      }
+    };
+  }
+};
+beforeEach(function() {
+  jasmine.addMatchers(regexMatchers);
+});
