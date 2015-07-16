c733b1e31e5d85c7f68d594fecca74e96d479123
Ryan Davis <ryan@mokeys.org>
2015-07-15 22:25:31 -0400

    ch09 solutions

--- src/ch09/ch09.spec.js
+++ src/ch09/ch09.spec.js
@@ -2,38 +2,38 @@ describe('ch09', function() {
 
   describe('ex01 regex golf', function() {
     it('car or cat', function() {
-      var re = null;
+      var re = /ca[rt]/;
       expect(re).toMatchAll("my car", "bad cats");
       expect(re).toMatchNone("camper", "high art");
     });
 
     it('pop and prop', function() {
-      var re = null;
+      var re = /pr?op/;
       expect(re).toMatchAll("pop culture", "mad props");
       expect(re).toMatchNone("plop");
     });
     it('ferret, ferry, and ferrari', function() {
-      var re = null;
+      var re = /ferr(et|y|ari)/;
       expect(re).toMatchAll("ferret", "ferry", "ferrari");
       expect(re).toMatchNone("ferrum", "transfer A");
     });
     it('Any word ending in ious', function() {
-      var re = null;
+      var re = /\wious\b/;
       expect(re).toMatchAll("how delicious", "spacious room");
       expect(re).toMatchNone("ruinous", "consciousness");
     });
     it('A whitespace character followed by a dot, comma, colon, or semicolon', function() {
-      var re = null;
+      var re = /\s[.,:;]/;
       expect(re).toMatchAll("bad punctuation .");
       expect(re).toMatchNone("escape the dot");
     });
     it('A word longer than six letters', function() {
-      var re = null;
+      var re = /\w{7,}/;
       expect(re).toMatchAll("hottentottententen");
       expect(re).toMatchNone("no", "hotten totten tenten");
     });
-        it('A word without the letter e', function() {
-      var re = null;
+    it('A word without the letter e', function() {
+      var re = /\b[^e ]+\b/;
       expect(re).toMatchAll("red platypus", "wobbling nest");
       expect(re).toMatchNone("earth bed", "learning ape");
     });
@@ -41,8 +41,8 @@ describe('ch09', function() {
 
   it('swaps quotes', function() {
     var text = "'I'm the cook,' he said, 'it's my job.'",
-        re = /A/g,
-        replacement = "B"
+        re = /'(.+?)'(\W|$)/g,
+        replacement = '"$1"$2'
     ;
     expect(text.replace(re, replacement))
       .toBe('"I\'m the cook," he said, "it\'s my job."');
@@ -50,7 +50,7 @@ describe('ch09', function() {
   });
 
   it('matches numbers', function() {
-    var re = null;
+    var re = /^[+\-]?(\.?\d|\d+\.?)\d*(e[+\-]?\d+)?$/i;
     expect(re).toMatchAll("1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
                           "1e+12");
     expect(re).toMatchNone("1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5",
